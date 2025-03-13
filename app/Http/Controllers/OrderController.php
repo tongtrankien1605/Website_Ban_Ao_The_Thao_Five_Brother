4175a4ef229571cfe83a681754b14792f36d8bd5
tongtrankien1605 <tongtrankien1605@gmail.com>
2025-03-13 21:27:35 +0700

    order details 40%

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -48,10 +48,41 @@ class OrderController extends Controller
         if ($request->payment_method == 1) {
             $cart->delete();
             // $order->update(['id_order_status' => 2]); // "Đã thanh toán"
-            return redirect()->route('order.success')->with('success', 'Đơn hàng của bạn sẽ được giao COD!');
+            return redirect()->route('index')->with('success', 'Đơn hàng của bạn sẽ được giao COD!');
         }
 
         $paymentController = new PaymentController();
         return $paymentController->processPayment($request, $order);
+    }
+
+
+
+    // Order Details
+    public function getOrderDetails($id)
+    {
+        $order = Order::where('id', $id)
+            ->with(['orderDetails' => function ($query) {
+                $query->join('skuses', 'skuses.id', '=', 'order_details.id_product_variant')
+                    ->select('order_details.*', 'skuses.name');
+            }])
+            ->first();
+
+        if (!$order) {
+            return response()->json(['error' => 'Order not found'], 404);
+        }
+
+        return response()->json([
+            'id' => $order->id,
+            'date' => $order->created_at->format('d/m/Y'),
+            'status' => $order->payment_method_status_name,
+            'total' => number_format($order->total_amount),
+            'items' => $order->orderDetails->map(function ($item) {
+                return [
+                    'name' => $item->name,
+                    'quantity' => $item->quantity,
+                    'price' => number_format($item->price),
+                ];
+            }),
+        ]);
     }
 }
